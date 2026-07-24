73c04ab308b2f1ed670c10f5487a8cf9559bb0fd
sanaullah-devopes <sanaullah963741@gmail.com>
2026-07-24 05:58:51 -0700

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let mode = document.querySelector("#mode");
+
 let curmode = "light";
-function check() {
+
+mode.addEventListener("click", () => {
   if (curmode == "light") {
     curmode = "dark";
     document.querySelector(".calculator").style.backgroundColor = "black";
@@ -9,32 +11,34 @@ function check() {
     document.querySelector(".calculator").style.backgroundColor = "white";
   }
   console.log(curmode);
-}
-mode.addEventListener("click", check);
+});
 
-let operator = document.querySelector("#mode");
-function col() {
+mode.addEventListener("click", () => {
   const operators = document.querySelectorAll(".operator");
-
   operators.forEach((button) => {
     if (curmode === "dark") {
       button.style.backgroundColor = "white";
       button.style.color = "#afafaf";
+      document.querySelector(".input").style.color = "white";
     } else {
       button.style.backgroundColor = "#afafaf";
       button.style.color = "white";
+      document.querySelector(".input").style.color = "black";
     }
   });
-}
-operator.addEventListener("click", col);
-function change() {
-  if (curmode == "dark") {
-    document.querySelector(".input").style.color = "white";
-  } else {
-    document.querySelector(".input").style.color = "black";
-  }
-}
-mode.addEventListener("click", change);
-function clearDisplay() {
-  display.value = "";
-}
+});
+
+const display = document.querySelector(".input");
+const buttons = document.querySelectorAll(".buttons");
+
+let expression = "";
+let calculate = (value) => {};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("value");
+
+    expression += value;
+    display.value = expression;
+  });
+});
